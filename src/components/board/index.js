db9ccdf6c0683c2d9a18d45c83d5ffc3d6de009b
asg0828 <asg0828@naver.com>
2020-11-21 23:27:17 +0900

    테이블 생성

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -1,8 +1,96 @@
+import React, {
+  useState,
+  useEffect
+}
+from 'react';
+
 const Board = () => {
-  
+  const [isReg, setIsReg] = useState(false);
+  const [userList, setUserList] = useState([]);
+  const [userInfo, setUserInfo] = useState({
+    id: '',
+    password: '',
+    name: ''
+  });
+
+  // useEffect(() => {
+
+  // },[userInfo])
+
+  const handleValue = (e) => {
+    setUserInfo({
+      ...userInfo,
+      [e.target.name]: e.target.value
+    })
+    console.log(e.target.value);
+    console.log(userInfo);
+  }
+
+  const userSave = () => {
+    setUserInfo([
+      ...userList,
+      userInfo
+    ])
+  }
+
+  const User = () => {
+    if(isReg){
+      return (
+        <>
+          <table>
+            <tr>
+              <th>아이디</th>
+              <td>
+                <input name="id" onChange={handleValue} value={userInfo.id}/>
+              </td>
+            </tr>
+            <tr>
+              <th>비밀번호</th>
+              <td>
+                <input name="password" onChange={handleValue} value={userInfo.password}/>
+              </td>
+            </tr>
+            <tr>
+              <th>이름</th>
+              <td>
+                <input name="name" onChange={handleValue} value={userInfo.name}/>
+              </td>
+            </tr>
+          </table>
+          
+          <button
+            onClick={userSave}
+          >확인
+          </button>
+          <button
+            onClick={regClose}
+          >닫기
+          </button>
+        </>
+      )
+    }
+    return null;
+  }
+
+  const regClose = () => {
+    setIsReg(false);
+  }
+
+  const regOpen = () => {
+    setIsReg(true);
+  }
+
   return (
-    <div>게시판</div>
+  <>
+    <div>테이블</div>
+    <button
+      onClick={regOpen}
+    >등록
+    </button>
+    <User></User>
+  </>
   )
+
 }
 
 export default Board;
